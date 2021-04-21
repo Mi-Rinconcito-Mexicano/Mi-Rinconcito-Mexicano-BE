71d52291c103d258b414e51bcd64f71184c5d905
Roberto Basulto <roberto.basulto085@gmail.com>
2021-04-20 23:48:19 -0400

    Added more tests to menu items

--- spec/requests/api/v1/menu/menu_item_request_spec.rb
+++ spec/requests/api/v1/menu/menu_item_request_spec.rb
@@ -7,23 +7,39 @@ describe 'Menu API' do
             description: 'Testing',
             price: 11.95,
             menu_category_id: combo_cat_id,
-            extra: [{info: 'Half or Whole beef', price: 2}, {info: 'Any Change', price: 1}])
-            
-        require 'pry'; binding.pry 
+            extra: [{info: "Half or Whole beef", price: 2}, {info: 'Any Change', price: 1}])
+
         taco_cat_id = MenuCategory.create(name: 'Tacos', description: 'Order of three corn tortillas with onions and cilantro').id
         @chicken_taco = MenuItem.create(name: 'Pollo / Chicken', price: 5.95, menu_category_id: taco_cat_id)
 
         appetizer_cat_id = MenuCategory.create(name: 'Appetizers').id
-        guacamole_picante = MenuItem.create(name: 'Guacamole Picante', price: 7.50, tags: ['vegeterian', 'hot', 'vegan'], menu_category_id: appetizer_cat_id)
+        @guacamole_picante = MenuItem.create(name: 'Guacamole Picante', price: 7.50, tags: ['vegeterian', 'hot', 'vegan'], menu_category_id: appetizer_cat_id)
     end
 
     it 'can send all menu items' do
-      get '/api/vi/menu'
+        get '/api/v1/menu'
 
-      expect(response).to be_successfull
+        expect(response).to be_successful
+        menu_items = JSON.parse(response.body, symbolize_names: true)
 
-      menu_items = JSON.parse(response.body, symbolize_names: true)
+        expect(menu_items.count).to eq(3)
+        expect(menu_items[0][:id]).to eq(@combination.id)
+        expect(menu_items[1][:id]).to eq(@chicken_taco.id) 
+        expect(menu_items[2][:id]).to eq(@guacamole_picante.id) 
+    end
 
-      expect(menu_items.count).to eq(3) 
+    it 'extras and tags are arrays' do
+        get '/api/v1/menu'
+
+        expect(response).to be_successful
+        menu_items = JSON.parse(response.body, symbolize_names: true)
+
+        expect(menu_items[0][:extra]).to be_a(Array)
+        expect(menu_items[0][:tags]).to be_a(Array)
+        expect(menu_items[0][:extra][0]).to be_a(Hash)
+        expect(menu_items[1][:extra]).to be_a(Array)
+        expect(menu_items[1][:tags]).to be_a(Array)
+        expect(menu_items[2][:extra]).to be_a(Array)
+        expect(menu_items[2][:tags]).to be_a(Array)
     end
 end
