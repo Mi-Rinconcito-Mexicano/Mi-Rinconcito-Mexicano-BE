47f6f59d514d45b6466309b392d92087ec97c141
Roberto Basulto <roberto.basulto085@gmail.com>
2021-04-22 16:45:57 -0400

    Refactored category mapping to facade

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::MenuController < ApplicationController
     def index
-        menu = MenuCategory.all.map do |menu_cat|
-            Menu.new(menu_cat)
-        end
-        
-        render json: MenuSerializer.new(menu)
+        render json: MenuSerializer.new(MenuFacade.fetch_menu)
     end
 end
