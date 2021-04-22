cde4d1024f3235144b6ff342b7953bbd4bde9b00
Roberto Basulto <roberto.basulto085@gmail.com>
2021-04-22 16:46:16 -0400

    Updated poro to no longer make a db call

--- app/poros/menu.rb
+++ app/poros/menu.rb
@@ -1,9 +1,9 @@
 class Menu
     attr_reader :id, :category, :description, :category_items
-    def initialize(menu_category)
+    def initialize(menu_category, category_items)
         @id = menu_category.id
         @category = menu_category.category
         @description = menu_category.description
-        @category_items = menu_category.menu_items
+        @category_items = category_items
     end
 end
