89237de9e5b9d5370e7412f0479211b578872f4a
Roberto Basulto <roberto.basulto085@gmail.com>
2021-04-22 00:13:48 -0400

    updated the schema to use category instead of name for the categories

--- db/migrate/20210421011915_create_menu_categories.rb
+++ db/migrate/20210421011915_create_menu_categories.rb
@@ -1,7 +1,7 @@
 class CreateMenuCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :menu_categories do |t|
-      t.string :name
+      t.string :category
       t.string :description, null: true
 
       t.timestamps
