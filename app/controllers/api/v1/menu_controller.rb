c6317dba8eb700220804149414e8616ba3f3336b
Roberto Basulto <roberto.basulto085@gmail.com>
2021-04-22 15:52:09 -0400

    Updated menu controller with serializer

--- app/controllers/api/v1/menu_controller.rb
+++ app/controllers/api/v1/menu_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::MenuController < ApplicationController
     def index
-        render json: MenuItem.all
+        menu = MenuCategory.all.map do |menu_cat|
+            Menu.new(menu_cat)
+        end
+        
+        render json: MenuSerializer.new(menu)
     end
 end
